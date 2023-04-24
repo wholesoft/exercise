1ea56252577f031781fc894e1d10cb914c7011e1
Erik <69864741+wholesoft@users.noreply.github.com>
2023-04-24 09:04:13 -0700

    updates

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,73 @@
+import { NextResponse } from "next/server"
+
+const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos"
+
+const API_KEY: string = process.env.DATA_API_KEY as string
+
+export async function GET(request: Request) {
+  const res = await fetch(DATA_SOURCE_URL)
+  const origin = request.headers.get("origin")
+  const todos: Todo[] = await res.json()
+
+  return new NextResponse(JSON.stringify(todos), {
+    headers: {
+      "Access-Control-Allow-Origin": origin || "*",
+      "Content-Type": "application/json",
+    },
+  })
+}
+
+export async function DELETE(request: Request) {
+  const { id }: Partial<Todo> = await request.json()
+  if (!id) return NextResponse.json({ message: "Todo id required" })
+  await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": API_KEY,
+    },
+  })
+  return NextResponse.json({ message: `Todo ${id} deleted` })
+}
+
+export async function POST(request: Request) {
+  const { userId, title }: Partial<Todo> = await request.json()
+  if (!userId || !title)
+    return NextResponse.json({ message: "Missing required data." })
+  const res = await fetch(DATA_SOURCE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": API_KEY,
+    },
+    body: JSON.stringify({
+      userId,
+      title,
+      completed: false,
+    }),
+  })
+  const newTodo: Todo = await res.json()
+
+  return NextResponse.json(newTodo)
+}
+
+export async function PUT(request: Request) {
+  const { userId, id, title, completed }: Todo = await request.json()
+  if (!userId || !title || !id || typeof completed !== "boolean")
+    return NextResponse.json({ message: "Missing required data." })
+  const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": API_KEY,
+    },
+    body: JSON.stringify({
+      userId,
+      title,
+      completed,
+    }),
+  })
+  const updatedTodo: Todo = await res.json()
+
+  return NextResponse.json(updatedTodo)
+}
---
+++ src/middleware.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server"
+
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? ["https://strength.wholesoft.net"]
+    : ["https://localhost:3000", "http://127.0.0.1"]
+
+export function middleware(request: Request) {
+  const origin = request.headers.get("origin")
+  console.log("ORIGIN IS:")
+  console.log(origin)
+
+  // origin seems to be null if accessing locally
+  //if (origin && !allowedOrigins.includes(origin) || !origin) {
+  if (origin && !allowedOrigins.includes(origin)) {
+    return new NextResponse(null, {
+      status: 400,
+      statusText: "Bad Request",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    })
+  }
+
+  /*   const regex = new RegExp("/api/*")
+
+  if (request.url.includes("/api")) {
+  }
+
+  if (regex.test(request.url) {
+  } */
+
+  console.log("Middleware!")
+  console.log(request.method)
+  console.log(request.url)
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: "/api/:path*",
+}
